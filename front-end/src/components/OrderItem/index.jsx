c420b3a4e409c6d4dcbca7bba9c14ff9d8dbbf29
Ruy Barbosa Jr <ruybarbosajr@pop-os.localdomain>
2022-08-18 17:47:21 -0300

    fix: page seller/orders/:id

--- front-end/src/components/OrderItem/index.jsx
+++ front-end/src/components/OrderItem/index.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import UserContext from '../../context/UserContext';
 
 export default function OrderItem({ id, status, data, totalPrice }) {
+  const { userData: { role } } = useContext(UserContext);
   return (
     <div>
-      <Link to={ `/customer/orders/${id}` }>
-        <p data-testid={ `customer_orders__element-order-id-${id}` }>
+      <Link to={ `/${role}/orders/${id}` }>
+        <p data-testid={ `${role}_orders__element-order-id-${id}` }>
           Pedido:
           {id}
         </p>
         <p
-          data-testid={ `customer_orders__element-delivery-status-${id}` }
+          data-testid={ `${role}_orders__element-delivery-status-${id}` }
         >
           {status}
         </p>
         <p
-          data-testid={ `customer_orders__element-order-date-${id}` }
+          data-testid={ `${role}_orders__element-order-date-${id}` }
         >
           {data.split('-')[2].slice(0, 2)}
           /
@@ -25,7 +27,7 @@ export default function OrderItem({ id, status, data, totalPrice }) {
           {data.split('-')[0]}
         </p>
         <p
-          data-testid={ `customer_orders__element-card-price-${id}` }
+          data-testid={ `${role}_orders__element-card-price-${id}` }
         >
           {totalPrice.replace('.', ',')}
 
